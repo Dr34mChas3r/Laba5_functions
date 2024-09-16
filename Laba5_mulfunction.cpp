dbab9303dc0d6354baf0574cc37dde874a3dd68c
Dr34mChas3r <53311619+Dr34mChas3r@users.noreply.github.com>
2024-09-16 22:35:47 +0300

    easy

--- Laba5_mulfunction.cpp
+++ Laba5_mulfunction.cpp
@@ -1,7 +1,23 @@
-// Напишіть функцію cubeVolume, яка приймає дов-жину ребра куба і повертає його об'єм. 
+// Напишіть функцію cubeVolume, яка приймає 
+// довжину ребра куба і повертає його об'єм. 
+
 #include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int cubeVolume(int side);
 
 int main()
 {
-  
+	int side;
+	cout << "Введіть довжину ребра куба: ";
+	cin >> side;
+	int result = cubeVolume(side);
+	cout << "Об'єм куба становить " << result << " метрів кубічних.";
+	return 0;
+}
+
+int cubeVolume(int side) {
+	return pow(side, 3);
 }
